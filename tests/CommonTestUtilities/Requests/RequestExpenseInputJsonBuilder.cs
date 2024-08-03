e92091204779f2894f470652f9e17e8f0680a4fb
Anderson Paiva <anderson.works1998@gmail.com>
2024-08-03 17:19:17 -0300

    feat: teste de unidade

---
+++ tests/CommonTestUtilities/Requests/RequestExpenseInputJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestExpenseInputJsonBuilder
+{
+    public static RequestExpenseInputJson Build()
+    {
+        return new Faker<RequestExpenseInputJson>()
+            .RuleFor(input => input.Title, faker => faker.Commerce.Product())
+            .RuleFor(input => input.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(input => input.Date, faker => faker.Date.Past())
+            .RuleFor(input => input.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(input => input.Amount, faker => faker.Random.Decimal(min: 1, max: 1000));
+    }
+}
---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,87 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact(DisplayName = nameof(Success))]
+    [Trait("Validators", "Expense Register - Validator")]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var input = RequestExpenseInputJsonBuilder.Build();
+
+        //Act
+        var result = validator.Validate(input);
+
+        //Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory(DisplayName = nameof(ErrorWhenTitleIsInvalid))]
+    [Trait("Validators", "Expense Register - Validator")]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("    ")]
+    public void ErrorWhenTitleIsInvalid(string? title)
+    {
+        var validator = new RegisterExpenseValidator();
+        var input = RequestExpenseInputJsonBuilder.Build();
+        input.Title = title;
+
+        var result = validator.Validate(input);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+    }
+
+    [Fact(DisplayName = nameof(ErrorWhenDateFuture))]
+    [Trait("Validators", "Expense Register - Validator")]
+    public void ErrorWhenDateFuture()
+    {
+        var validator = new RegisterExpenseValidator();
+        var input = RequestExpenseInputJsonBuilder.Build();
+        input.Date = DateTime.UtcNow.AddDays(1);
+
+        var result = validator.Validate(input);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE));
+    }
+
+    [Fact(DisplayName = nameof(ErrorWhenPaymentTypeIsInvalid))]
+    [Trait("Validators", "Expense Register - Validator")]
+    public void ErrorWhenPaymentTypeIsInvalid()
+    {
+        var validator = new RegisterExpenseValidator();
+        var input = RequestExpenseInputJsonBuilder.Build();
+        input.PaymentType = (PaymentType)100;
+
+        var result = validator.Validate(input);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_INVALID));
+    }
+
+    [Theory(DisplayName = nameof(ErrorWhenAmountIsInvalid))]
+    [Trait("Validators", "Expense Register - Validator")]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-5)]
+    public void ErrorWhenAmountIsInvalid(decimal amount)
+    {
+        var validator = new RegisterExpenseValidator();
+        var input = RequestExpenseInputJsonBuilder.Build();
+        input.Amount = amount;
+
+        var result = validator.Validate(input);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
+    }
+}
